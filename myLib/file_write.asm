012a84be4f40e32fc93c4439681fea4216ca245f
Theodor <warpmines@gmail.com>
2025-03-24 19:30:41 +0100

    made file_write

---
+++ myLib/file_write.asm
@@ -0,0 +1,34 @@
+%ifndef FILE_WRITE
+%define FILE_WRITE
+
+
+; int {rax} fileWrite(fd {rdi}, char* {rsi}, size_t {rdx})
+;   takes file descriptor in {rax} and char pointer in {rbx} and the amount of data to write in {rcx}
+;   returns the amount of written data. 
+_fileWrite:
+    push rax
+
+    mov rax, 1 ; sys_write
+    syscall
+    
+    pop rax
+    ret
+
+
+%macro file_write 3
+    push rdi
+    push rsi
+    push rdx
+
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    call _fileWrite
+
+    pop rdi
+    pop rsi
+    pop rdx
+
+%endmacro
+
+%endif
